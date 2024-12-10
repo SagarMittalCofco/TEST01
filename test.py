3ffaaa33b58b84dc1ace4bb2e05fec08e96ee4e7
SagarMittalCofco <SagarMittal@cofcointernational.com>
2024-12-10 12:27:51 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,42 @@
+'''a=5;b=7; print(a*b)
+a,b=[8,6]; print(a*b)
+
+c=17
+c+=3
+print(c)
+
+def MyCalculator(no1,no2):
+    print(no1*no2)
+
+print(MyCalculator(7,9),c)
+'''
+
+#a= list(map(int,input('enter string ').split()))
+#print(type(a))
+
+#a = 4+5/2+7*8
+#print(a)
+
+'''d = [1,2,3,4,5]
+for i in d:   
+    if i == 4:
+        continue
+    print(i)
+    '''
+'''
+MyList = [10,20,30,40,50]
+MyList.append(60)
+MyList(1) = 10
+print(MyList)
+MyList.remove(60)
+print(MyList)
+'''
+'''
+import random
+print(random.random())
+print(random.uniform(1.1,7.8))
+MyList = (1,2,5,5,'hello')
+print(random.choice(MyList))
+'''
+
+
